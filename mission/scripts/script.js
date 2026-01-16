44510980d3cda61f63c4f4dc01332a475b616595
Carter <beauduke2014@gmail.com>
2026-01-15 20:08:04 -0700

    finishing the dark and light mode drop down for mission

--- mission/scripts/script.js
+++ mission/scripts/script.js
@@ -4,12 +4,32 @@ let logo = document.querySelector('img');
 
 selectElem.addEventListener('change', changeTheme);
 
+let image = document.querySelector("img");
+
+let mission = document.querySelector(".mission");
+
+let line = document.querySelector("hr");
+
+let byui = document.querySelector("h2");
+
 function changeTheme() {
     let current = selectElem.value;
-    if (current == 'dark') {
-        // code for changes to colors and logo
+    if (current === 'dark') {
+        document.body.style.backgroundColor = "#404040";
+        document.body.style.color = "white";
+        image.setAttribute("src", "images/byui-logo-white.png");
+        mission.style.borderColor =  "white"; 
+        line.style.backgroundColor = "white";
+        byui.style.color = "#9cd6ff";
+        
     } else {
-        // code for changes to colors and logo
+        document.body.style.backgroundColor = "";
+        document.body.style.color = "";
+        image.setAttribute("src", "images/byui-logo-blue.webp")
+        mission.style.borderColor = "";
+        line.style.backgroundColor = "";
+        byui.style.color = "";
+        
     }
 }           
                     
