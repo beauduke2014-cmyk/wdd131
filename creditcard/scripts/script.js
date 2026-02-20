42b2ce7def5a28ce66d65d6b1065f81550b283f5
Carter <beauduke2014@gmail.com>
2026-02-19 19:27:04 -0700

    finish credit card activity js

---
+++ creditcard/scripts/script.js
@@ -0,0 +1,40 @@
+
+function displayError(msg) {
+    document.querySelector('.errors').textContent = msg
+}
+
+
+function isCardNumberValid(number) {
+	// normally we would contact a credit card service...but we don't know how to do that yet. So to keep things simple we will only accept one number
+	return number === '1234123412341234'
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+    let errorMsg = '';
+    displayError('');
+
+    let cardNumber = document.querySelector('#creditCardNumber');
+    const cardNum = cardNumber.value.trim();
+    if (paymentSelect.value === 'creditCard') {
+        if (!/^\d{16}$/.test(cardNum)) {
+            errorMsg += 'Card number must be 16 digits\n';
+        } else if(!isCardNumberValid(cardNum)) {
+            errorMsg += 'Card number is not valid\n';
+        }
+        const expYear = Number(document.querySelector('#year').value);
+        const expMonth = Number(document.querySelector('#month').value);
+        const currentDate = new Date()
+
+        if (2000 + expYear < currentDate.getFullYear() || (2000 + expYear === currentDate.getFullYear() && expMonth <= (currentDate.getMonth()))) {
+            errorMsg += 'Card is expired\n';
+        }
+    }
+
+    if (errerMsg !== '') {
+        displayError(errorMsg)
+        return;
+    }
+    const formContainer = document.getElementById('checkoutForm');
+    formContainer.innerHTML = '<h2>Thank you for your purchase.</h2>';
+}
